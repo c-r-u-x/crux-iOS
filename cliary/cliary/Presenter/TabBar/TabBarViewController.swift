37203423660bb8f371d008265513ada5c2489b94
Eunbin Kwon <kwoneunbini@gmail.com>
2024-06-06 16:51:08 +0900

    feat: create base tabbar controller #2

--- cliary/cliary/Presenter/TabBar/TabBarViewController.swift
+++ cliary/cliary/Presenter/TabBar/TabBarViewController.swift
@@ -8,22 +8,87 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureTabBarViewController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // TODO: - Design should be adopted.
+    private func configureTabBarViewController() {
+        self.view.backgroundColor = .white
+        self.tabBar.backgroundColor = .systemBackground
+        self.viewControllers = [
+            navigationController(of: Tab.daily),
+            navigationController(of: Tab.camera),
+            navigationController(of: Tab.profile),
+        ]
     }
-    */
+    
+    private func navigationController(of tab: Tab) -> UINavigationController {
+        let navigationController = NavigationController(rootViewController: tab.viewController)
+        
+        navigationController.tabBarItem.title = tab.title
+        navigationController.tabBarItem.image = tab.icon
+        navigationController.tabBarItem.selectedImage = tab.selectedIcon
+        navigationController.navigationBar.isHidden = tab == .camera
+        
+        return navigationController
+    }
+}
 
+extension TabBarViewController {
+    enum Tab {
+        case daily
+        case camera
+        case profile
+        
+        // TODO: - Return the matched UIViewController after define those VCs.
+        var viewController: UIViewController {
+            switch self {
+            case .daily:
+                UIViewController().then { $0.view.backgroundColor = .red }
+            case .camera:
+                UIViewController().then { $0.view.backgroundColor = .green }
+            case .profile:
+                UIViewController().then { $0.view.backgroundColor = .blue }
+            }
+        }
+        
+        var title: String {
+            switch self {
+            case .daily:
+                "기록"
+            case .camera:
+                "촬영"
+            case .profile:
+                "프로필"
+            }
+        }
+        
+        // TODO: - Replace with the designed icon
+        var icon: UIImage? {
+            switch self {
+            case .daily:
+                UIImage(systemName: "calendar")
+            case .camera:
+                UIImage(systemName: "camera")
+            case .profile:
+                UIImage(systemName: "person")
+            }
+        }
+        
+        // TODO: - Replace with the designed icon
+        var selectedIcon: UIImage? {
+            switch self {
+            case .daily:
+                UIImage(systemName: "calendar")
+            case .camera:
+                UIImage(systemName: "camera.fill")
+            case .profile:
+                UIImage(systemName: "person.fill")
+            }
+        }
+    }
 }
