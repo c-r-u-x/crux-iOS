2ced1d397cb63b7f697b181402610f3529f79602
2unbini <75126613+2unbini@users.noreply.github.com>
2024-06-07 16:51:32 +0900

    Revert "[chore] created pr template"

--- cliary/cliary/App/SceneDelegate.swift
+++ cliary/cliary/App/SceneDelegate.swift
@@ -18,11 +18,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene)
         else { return }
-        let rootViewController = ViewController()
+//        let rootViewController = LoginViewController()
+        let rootViewController = TabBarViewController()
         
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
         window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
