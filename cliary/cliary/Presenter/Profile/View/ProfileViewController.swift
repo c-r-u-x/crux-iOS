6f2ee1fb1f7b3ee89905e8d582234cf32dcb88c2
Rob-Yoo <jin12243@naver.com>
2024-06-30 18:13:43 +0900

    Feat: BaseViewController와 BaseView를 활용한 초기 VC와 RootView 생성

---
+++ cliary/cliary/Presenter/Profile/View/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  cliary
+//
+//  Created by Jinyoung Yoo on 6/30/24.
+//
+
+import UIKit
+
+final class ProfileViewController: BaseViewController<ProfileRootView> {
+    override init(contentView: ProfileRootView) {
+        super.init(contentView: contentView)
+    }
+}
